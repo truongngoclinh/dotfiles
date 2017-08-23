31b7804d5b26bd5e367cb58691cca5427a9a8fe2
Linh <Linh@Truong.local>
2017-08-23 14:47:08 +0700

    update dotfiles, date: 23/08/2017

--- Scripts/update_dotfiles.sh
+++ Scripts/update_dotfiles.sh
@@ -1,7 +1,7 @@
 # script for updating dotfiles to github
 now="$(date +%d/%m/%Y)"
-echo "\033[0;91m =>> Starting... [0m"
-echo "\033[0;33m ==> Moving dotfiles to local repo [0m"
+echo "\033[0;91m =>> Starting... \033[0m"
+echo "\033[0;33m ==> Moving dotfiles to local repo \033[0m"
 
 # config files
 cp /Users/Truong/.vimrc /Users/Truong/WSP/github/dotfiles/
@@ -12,20 +12,20 @@ cp /Users/Truong/Script/update_dotfiles.sh /Users/Truong/WSP/github/dotfiles/Scr
 
 cd /Users/Truong/WSP/github/dotfiles/
 
-echo "\033[0;33m ==> Fetching origin changes [0m"
+echo "\033[0;33m ==> Fetching origin changes \033[0m"
 git pull 
 
-echo "\033[0;33m ==> Files changes status [0m"
+echo "\033[0;33m ==> Files changes status \033[0m"
 git status 
 
-echo "\033[0;33m ==> Adding files [0m"
+echo "\033[0;33m ==> Adding files \033[0m"
 git add --all
 git status
 
-echo "\033[0;33m ==> Committing files [0m"
+echo "\033[0;33m ==> Committing files \033[0m"
 git commit -m "update dotfiles, date: $now"
 
-echo "\033[0;33m ==> Pushing to github [0m"
+echo "\033[0;33m ==> Pushing to github \033[0m"
 git push origin master
 
-echo "\033[0;91m ==> Finished! [0m"
+echo "\033[0;91m ==> Finished! \033[0m"
