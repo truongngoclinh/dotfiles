33f7e7a3c427ebdea139ba9ffe835a271de7bbc3
Linh <Linh@Truong.local>
2017-09-25 14:00:44 +0700

    update dotfiles, date: 25/09/2017 - 14:00:43

--- Scripts/update_lang_ac_android.sh
+++ Scripts/update_lang_ac_android.sh
@@ -1,4 +1,4 @@
 echo "\x1B[32m Updating airpay counter language android version...\x1B[0m"
-cd Users/Truong/WSP/GARENA/SG/cyberpay-admin-app/misc/scripts
+cd /Users/Truong/WSP/GARENA/SG/cyberpay-admin-app/misc/scripts
 sh download_transify.sh
 echo "\x1B[32m Finished!\x1B[0m"
