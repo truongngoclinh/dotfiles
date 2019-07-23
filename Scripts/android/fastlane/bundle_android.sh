cc570fc7df4f19f8df188ae8090b89ca482037cd
ngoclinh.truong <ngoclinh.truong@ved.com.vn>
2019-07-23 14:26:35 +0700

    remove redundant scripts

--- Scripts/android/fastlane/bundle_android.sh
+++
@@ -1,3 +0,0 @@
-#/bin/bash
-
-cd ../../ocha-crm-rn && react-native bundle --platform android --dev false --entry-file index.dev.js --bundle-output ../app/src/main/assets/index.bundle --assets-dest ../app/src/main/res
--- Scripts/ios/bundle_ios.sh
+++
@@ -1,3 +0,0 @@
-#/bin/bash
-
-cd ../ocha-crm-rn && react-native bundle --platform ios --dev false --entry-file index.dev.js --bundle-output ../OchaCRM/ReactNative/JSBundle/main.jsbundle --assets-dest ../OchaCRM/ReactNative/JSBundle
